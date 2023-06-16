e561762d71482922ed73fd830ab9febe26453970
hamed donyavi <85428934+donyavirad@users.noreply.github.com>
2023-06-16 10:56:42 +0330

    first commit

---
+++ api/bot.js
@@ -0,0 +1,18 @@
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx)=> {
+    ctx.reply("Hi!")
+})
+
+
+module.exports = async (request, response) => {
+    try {
+        await bot.handleUpdate(JSON.parse(request.body));
+        response.json( { statusCode: 200, body: "" });
+    } catch (e) {
+        console.error("error in handler:", e);
+        response.json( { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" });
+    }
+};
